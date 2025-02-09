04061db69884f38675f7ca45e8ac4db4187cb60c
gabpereirach <gabriel.pereira@heig-vd.ch>
2025-02-09 20:38:11 +0100

    Add menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { MainNav } from "@/components/main-nav";
 
 //title of app
 export const metadata: Metadata = {
@@ -15,6 +16,13 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body>
+        <div className="border-b">
+        <div className="flex h-16 items-center px-4">
+            <div className="flex-1 flex justify-center">
+              <MainNav />
+            </div>
+          </div>
+        </div>
         {children}
       </body>
     </html>
---
+++ src/app/schedule/page.tsx
@@ -0,0 +1,9 @@
+export default function SchedulePage() {
+  return (
+	<div className="p-8">
+      	<div className="max-w-6xl mx-auto">
+			<h1 className="text-3xl font-bold text-center mb-8">Agenda</h1>
+		</div>
+	</div>
+  );
+}
